8f72c31c16339fa7ee0e72aa54121e1287ac740d
Equipo 012 <Equipo 012@DESKTOP-F5GRAV0>
2024-03-11 18:46:38 -0500

    Inicio

---
+++ Parcial_1_Punto_1/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //Un vendedor recibe un sueldo base de $XXXX.Además, recibe una comisión del 10 % sobre el valor total de cada venta
+
+        Console.WriteLine("Hello, World!");
+    }
+}
